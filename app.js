36b51562e858319db956004f00fba6f55ed78cf6
Jared Gross <jagross@udel.edu>
2019-07-27 19:01:26 -0400

    Add user connect/disconnect messages

--- app.js
+++ app.js
@@ -12,11 +12,41 @@ app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
 
-io.on('connection', function(socket){
-  socket.on('chat message', function(msg){
+/************SOCKET MANAGEMENT****************/
+let allClients = [];
+let users = [];
+
+io.on('connection', onConnect);
+
+function onConnect(socket){
+  let userName;
+  allClients.push(socket);
+  //Welcome new users
+  socket.on('welcome', (msg, name)=>{
+    socket.broadcast.emit('welcome', msg);
+    userName = name;
+    users.push(userName);
+  });
+
+  //Recieves chat message and sends it out to all including sender
+  socket.on('chat message', (msg)=>{
     io.emit('chat message', msg);
   });
-});
+
+  //Disconnection
+  socket.on('disconnect', (reason)=>{
+      console.log(reason);
+      socket.broadcast.emit('leave', `${userName} left the chat`);
+      let i = allClients.indexOf(socket);
+      allClients.splice(i, 1);
+      users.splice(i, 1);
+  });
+  }
+
+
+
+
+
 
 
 http.listen(3000, function(){
