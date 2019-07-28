946c1713de656d98b357c4433a19d425ceb3cea0
Jared Gross <jagross@udel.edu>
2019-07-28 13:24:21 -0400

    Add disconnect as a stop typing event

--- app.js
+++ app.js
@@ -15,17 +15,21 @@ app.get('/', function(req, res){
 /************SOCKET MANAGEMENT****************/
 let allClients = [];
 let users = [];
+let typingUsers = [];
 
 io.on('connection', onConnect);
 
 function onConnect(socket){
   let userName;
   allClients.push(socket);
+  io.emit('userList', users);
+
   //Welcome new users
   socket.on('welcome', (msg, name)=>{
     socket.broadcast.emit('welcome', msg);
     userName = name;
     users.push(userName);
+    io.emit('userList', users);
   });
 
   //Recieves chat message and sends it out to all including sender
@@ -33,14 +37,51 @@ function onConnect(socket){
     io.emit('chat message', msg);
   });
 
+  //Recieves alert that a user is typing
+  socket.on('typing', (userName)=>{
+    typingUsers.push(userName);
+    userIsTypingOutput(socket, false);
+  });
+
+  //Recieves alert that user is done typing
+  socket.on('doneTyping', (userName)=>{
+    console.log('done typing');
+    let i = typingUsers.indexOf(userName);
+    typingUsers.splice(i, 1);
+    userIsTypingOutput(socket, true);
+  });
+
   //Disconnection
   socket.on('disconnect', (reason)=>{
       console.log(reason);
-      socket.broadcast.emit('leave', `${userName} left the chat`);
+      socket.broadcast.emit('userLeave', `${userName} left the chat`);
       let i = allClients.indexOf(socket);
       allClients.splice(i, 1);
       users.splice(i, 1);
+      //user done typing event
+      i = typingUsers.indexOf(userName);
+      if(i !== -1){
+        typingUsers.splice(i, 1);
+        userIsTypingOutput(socket, true);
+      }
+      //    ----
+      io.emit('userList', users);
   });
+  }
+
+  function userIsTypingOutput(socket, isDone){
+    if(typingUsers.length >= 2){
+      let outputString = typingUsers[0];
+      for(i = 1; i < typingUsers.length; i ++){
+        outputString += `, ${typingUsers[i]}`;
+      }
+      outputString += " are typing...";
+      socket.broadcast.emit('typing', outputString, typingUsers);
+    }else if(typingUsers.length === 1 && !isDone){
+      socket.broadcast.emit('typing', `${typingUsers[0]} is typing...`, typingUsers);
+    }else if(typingUsers.length <= 1){
+      socket.broadcast.emit('doneTyping');
+    }
   }
 
 
