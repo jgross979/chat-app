b5aaaca35a159d621ad59c848b63c3b8eb1c5c4d
Jared Gross <tuk12216@temple.edu>
2020-03-19 19:33:31 -0400

    Give all chat functions access to socket object

--- app.js
+++ app.js
@@ -58,72 +58,10 @@ let typingUsers = [];
 
 /************SOCKET MANAGEMENT****************/
 const Chatroom = require("./services/Chatroom");
-const main = new Chatroom('', io);
 
-main.listen();
-// io.on('connection', onConnect);
-//
-// function onConnect(socket){
-//   let userName;
-//   allClients.push(socket);
-//   io.emit('userList', users);
-//
-//   //Welcome new users
-//   socket.on('welcome', (msg, name)=>{
-//     socket.broadcast.emit('welcome', msg);
-//     userName = name;
-//     users.push(userName);
-//     io.emit('userList', users);
-//   });
-//
-//   //Recieves chat message and sends it out to all including sender
-//   socket.on('chat message', (msg)=>{
-//     io.emit('chat message', msg);
-//   });
-//
-//   //Recieves alert that a user is typing
-//   socket.on('typing', (userName)=>{
-//     typingUsers.push(userName);
-//     userIsTypingOutput(socket, false);
-//   });
-//
-//   //Recieves alert that user is done typing
-//   socket.on('doneTyping', (userName)=>{
-//     let i = typingUsers.indexOf(userName);
-//     typingUsers.splice(i, 1);
-//     userIsTypingOutput(socket, true);
-//   });
-//
-//   //Disconnection
-//   socket.on('disconnect', (reason)=>{
-//       socket.broadcast.emit('userLeave', `${userName} left the chat`);
-//       let i = allClients.indexOf(socket);
-//       allClients.splice(i, 1);
-//       users.splice(i, 1);
-//       //user done typing event
-//       i = typingUsers.indexOf(userName);
-//       if(i !== -1){
-//         typingUsers.splice(i, 1);
-//         userIsTypingOutput(socket, true);
-//       }
-//       //    ----
-//       io.emit('userList', users);
-//   });
-//   }
-//
-//   function userIsTypingOutput(socket, isDone){
-//     if(typingUsers.length >= 2){
-//       let outputString = typingUsers[0];
-//       for(i = 1; i < typingUsers.length; i ++){
-//         outputString += `, ${typingUsers[i]}`;
-//       }
-//       outputString += " are typing...";
-//       socket.broadcast.emit('typing', outputString, typingUsers);
-//     }else if(typingUsers.length === 1 && !isDone){
-//       socket.broadcast.emit('typing', `${typingUsers[0]} is typing...`, typingUsers);
-//     }else if(typingUsers.length <= 1){
-//       socket.broadcast.emit('doneTyping');
-//     }
-//   }
+//Start listening for connections
+const Mainroom = new Chatroom('', io);
+
+
 
 
